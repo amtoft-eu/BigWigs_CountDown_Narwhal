06a067ca732c3d5069d8d666c1f905a75e80036c
Lukas <s160068@student.dtu.dk>
2019-06-06 19:41:00 +0200

    First commit

---
+++ Core.lua
@@ -0,0 +1,14 @@
+BigWigsAPI:RegisterCountdown("Narwhal Serious", {
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\5.ogg",
+})
+BigWigsAPI:RegisterCountdown("Narwhal Better", {
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\5.ogg",
+})
