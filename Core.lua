fef640fbce671392da803a97f144a4d7762133da
Lukas <s160068@student.dtu.dk>
2019-06-13 19:10:26 +0200

    Test123

--- Core.lua
+++ Core.lua
@@ -5,6 +5,7 @@ BigWigsAPI:RegisterCountdown("Narwhal Serious", {
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\4.ogg",
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Serious\\5.ogg",
 })
+
 BigWigsAPI:RegisterCountdown("Narwhal Better", {
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\1.ogg",
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\2.ogg",
