5499840a65a3444836d859f908dd7dafb035d18e
LAD <dev@amtoft.eu>
2024-01-29 11:43:40 +0100

    Added many more sounds. Also you can now choose five kinds of Narwhal.

--- Core.lua
+++ Core.lua
@@ -13,3 +13,27 @@ BigWigsAPI:RegisterCountdown("Narwhal Better", {
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\4.ogg",
 	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Better\\5.ogg",
 })
+
+BigWigsAPI:RegisterCountdown("Narwhal Mekka 1", {
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka1\\1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka1\\2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka1\\3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka1\\4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka1\\5.ogg",
+})
+
+BigWigsAPI:RegisterCountdown("Narwhal Mekka 2", {
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka2\\1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka2\\2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka2\\3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka2\\4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Mekka2\\5.ogg",
+})
+
+BigWigsAPI:RegisterCountdown("Narwhal Wacky", {
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Wacky\\1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Wacky\\2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Wacky\\3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Wacky\\4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Narwhal\\Media\\Sounds\\Narwhal\\Wacky\\5.ogg",
+})
